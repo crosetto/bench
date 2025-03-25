32265452607d0e611c4c1c098c56dd832cd8eaa9
P. C. Crosetto <crosetto@itis.swiss>
2025-03-25 13:48:03 +0100

    removing warning, changing output

--- src/bench.cpp
+++ src/bench.cpp
@@ -4,19 +4,21 @@
 
 int main(){
 
+
   std::vector<std::int32_t> v(2<<25, 0); // 128 MiB
   volatile int val = 1;
+  int size = v.size();
 
-  for(int stride = 1; stride < v.size(); ++stride)
+  for(int stride = 1; stride < 128; ++stride)
     {
       auto start = std::chrono::high_resolution_clock::now();
-      for(int i=0; i<v.size(); ++i)
+      for(int i=0; i<size; ++i)
         v[(i*stride)%v.size()] = val;
       auto stop = std::chrono::high_resolution_clock::now();
       auto elapsed = (stop - start).count();
-      for(int i=0; i< elapsed/1.e7; ++i)
-        std::cout<<"*";
-      std::cout<<"\n";
-        // std::cout<<elapsed/1.e7<<"\n";
+      // for(int i=0; i< elapsed/1.e7; ++i)
+      //   std::cout<<"*";
+      // std::cout<<"\n";
+      std::cout<<elapsed/1.e7<<"\n";
     }
 }
